1420e9de99a7ecf393a0dc5ccd6e8b9efeee0dea
soohyunnn <soohyunnn@gmail.com>
2021-10-30 10:35:04 +0900

    7장.예외처리

--- src/main/java/com/phangli/houseUtils/controller/policy/BrokeragePolicyFactory.java
+++ src/main/java/com/phangli/houseUtils/controller/policy/BrokeragePolicyFactory.java
@@ -1,6 +1,8 @@
 package com.phangli.houseUtils.controller.policy;
 
 import com.phangli.houseUtils.constants.ActionType;
+import com.phangli.houseUtils.exception.ErrorCode;
+import com.phangli.houseUtils.exception.HouseUtilsException;
 
 
 public class BrokeragePolicyFactory {
@@ -12,7 +14,7 @@ public class BrokeragePolicyFactory {
             case PURCHASE:
                 return new PurchaseBrokeragePolicy();
             default:
-                throw new IllegalArgumentException("해당 ActionType에 대한 정책이 존재하지 않습니다.");
+                throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 ActionType에 대한 정책이 존재하지 않습니다.");
         }
     }
 
